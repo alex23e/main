eb20706abd887f638d3f0792709af0be7e415998
Rangershot <shelbyandrew87@gmail.com>
2024-05-27 23:37:04 +0330

    the code has been some misValues I Changed it. the Edit Day 1403/03/07 late Night.

--- MainActivity.java
+++ MainActivity.java
@@ -9,8 +9,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText edt1,edt2;
-    TextView txt1;
+    EditText num1,num2;
+    TextView out1;
     Button btnsum;
     String s;
     @Override
@@ -18,17 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txt1 = findViewById(R.id.txt1);
-        edt1 = findViewById(R.id.edt1);
-        edt2 = findViewById(R.id.edt2);
+        out1 = findViewById(R.id.txt1);
+        num1 = findViewById(R.id.edt1);
+        num2 = findViewById(R.id.edt2);
         btnsum = findViewById(R.id.btnsum);
 
         btnsum.setOnClickListener(v -> {
-            float a = Float.parseFloat(edt1.getText().toString());
-            float b = Float.parseFloat(edt2.getText().toString());
+            float a = Float.parseFloat(num1.getText().toString());
+            float b = Float.parseFloat(num2.getText().toString());
             float c = a+b;
             s=Float.toString(c);
-            txt1.setText(s);
+            out1.setText(s);
         });
     }
 }
+// the chages has been edited by Ali Shahbazi in some values (num, out).
